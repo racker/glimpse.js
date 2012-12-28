c439800b4ac9befed927dee7e5ddae94a0c39d0f
Ed Rooth <ed.rooth@gmail.com>
2012-12-28 15:23:45 -0800

    Animate in the lines

--- src/core/component/line.js
+++ src/core/component/line.js
@@ -18,7 +18,9 @@ function (d3, component) {
         'yScale': null,
         'strokeWidth': 1.5,
         'color': 'steelblue',
-        'fillArea': false
+        'fillArea': false,
+        'interpolate': 'linear',
+        'animationDuration': 500
       });
       return this;
     },
@@ -42,17 +44,48 @@ function (d3, component) {
         .y(function(d) {
           return that.yScale(that.y(d));
         })
-        .interpolate('linear');
+        .interpolate(this.interpolate);
 
       this.selection.append('path')
         .data([data])
         .attr({
+          'class': 'line',
           'stroke-width': this.strokeWidth,
           'stroke': this.color,
           'fill': 'none',
-          'opacity': 1,
-          'd': this.line
+          'opacity': 1
         });
+
+      // animate the drawing in
+      function animate (selection, pathFunc) {
+        var endIndex = 1,
+            len = data.length -1,
+            step = 1,
+            totalDuration = that.animationDuration,
+            stepDuration = Math.floor(totalDuration / len),
+            intervalId;
+
+        function draw (endIndex) {
+          selection.call(function (d) {
+            // this: path selection
+            this.attr('d', function (d) {
+                return pathFunc(d.slice(0, endIndex + 1));
+              });
+          });
+        }
+
+        intervalId = setInterval(function () {
+          draw(endIndex);
+          endIndex += step;
+          if (endIndex >= len) {
+            draw(len);
+            clearInterval(intervalId);
+          }
+        }, stepDuration);
+
+      }
+
+      animate(this.selection.select('.line'), this.line);
 
       // optionally draw the area
       if (this.fillArea) {
@@ -63,16 +96,18 @@ function (d3, component) {
           .y1(function(d) {
             return that.yScale(that.y(d));
           })
-          .y0(this.height);
+          .y0(this.height)
+          .interpolate(this.interpolate);
 
         this.selection.append('path')
           .data([data])
           .attr({
+            'class': 'area',
             'stroke': 'none',
             'fill': this.color,
-            'opacity': 0.4,
-            'd': this.area
+            'opacity': 0.4
           });
+        animate(this.selection.select('.area'), this.area);
       }
 
       return this;
