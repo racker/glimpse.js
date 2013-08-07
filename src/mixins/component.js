6c5d9d575aa7496e8a4d783a9d9c8fe492c759de
Johann Verghese <johann.verghese@gmail.com>
2013-08-06 17:13:26 -0700

    docs(mixins.component) Add docs for all functions in the component mixin.

--- src/mixins/component.js
+++ src/mixins/component.js
@@ -19,6 +19,9 @@ define([
       return this._.root;
     },
 
+    /**
+     * Initalises all common component mixins, properties etc
+     */
     init: function() {
       var _ = this._;
       _.globalPubsub = pubsub.getSingleton();
@@ -35,6 +38,9 @@ define([
         zIndex);
     },
 
+    /**
+     * A standard implementaion of the data setter/getter of the component.
+     */
     data: function(data) {
       var _ = this._;
       if (data) {
@@ -47,10 +53,20 @@ define([
       return _.dataCollection.get(_.config.dataId);
     },
 
+    /**
+     * Returns a pubsub scope that spans the entire graph.
+     * @param {String} The event name.
+     * @return {String} The scoped (prefixed) event name.
+     */
     globalScope: function(eventName) {
       return pubsub.scope(this._.config.rootId)(eventName);
     },
 
+    /**
+     * Returns a scope local to the component.
+     * @param {String} The event name.
+     * @return {String} The scoped (prefixed) event name.
+     */
     scope: function(eventName) {
       var scope = this._.config.rootId;
       if (this._.config.type !== 'graph') {
@@ -59,6 +75,9 @@ define([
       return pubsub.scope(scope)(eventName);
     },
 
+    /**
+     * A lifecyle method for cleaning up after the component.
+     */
     destroy: function() {
       var _ = this._;
       array.getArray(_.config.components).forEach(function(component) {
@@ -71,30 +90,56 @@ define([
       this._ = null;
     },
 
+    /**
+     * A lifecycle method for rendering the component.
+     */
     render: function() {
       // noop
       return this;
     },
 
+    /**
+     * Returns a boolean indicating whether the component is rendered.
+     * @return {Boolean}
+     */
     isRendered: function() {
       return !!this.root();
     },
 
+    /**
+     * A lifecycle method for updating a rendered component.
+     */
     update: function() {
       // noop
       return this;
     },
 
+    /**
+     * Event handler attacher.
+     * Note: The events are scoped local to the component.
+     * @param {String} eventName The event name
+     * @param {Function} callback
+     */
     on: function(eventName, callback) {
       var _ = this._;
       _.globalPubsub.sub(this.scope(eventName), callback);
     },
 
+    /**
+     * Event handler detacher.
+     * @param {String} eventName The event name
+     * @param {Function} callback
+     */
     off: function(eventName, callback) {
       var _ = this._;
       _.globalPubsub.unsub(this.scope(eventName), callback);
     },
 
+    /**
+     * Event emitter.
+     * @param {String} eventName The event name
+     * @param {...} arguments The arguments that are passed to the callback.
+     */
     emit: function(eventName) {
       var _ = this._,
           args = array.convertArgs(arguments);
