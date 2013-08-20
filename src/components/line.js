a358dd53d295e23d27a74b6abcb13363e74a574f
amrutac <amruta.chitnis@gmail.com>
2013-08-20 10:25:47 -0700

    fix(mixins.highlight) Adds transition for highlight on line component

--- src/components/line.js
+++ src/components/line.js
@@ -44,6 +44,8 @@ function(array, config, obj, fn, string, d3util, mixins, dataFns) {
       highlightRadius: 4,
       highlightFill: '#fff',
       highlightStrokeWidth: 2,
+      highlightTransDuration: 500,
+      highlightTransDelay: 1000,
       showHighlight: false
     };
 
